5eeafcb80f33505c92ce70fd3014425004adda67
QiuYuer <zcabyqi@ucl.ac.uk>
2021-03-16 21:25:08 +0000

    search modification

--- ShowMovieList.php
+++ ShowMovieList.php
@@ -1,5 +1,5 @@
 <?php
-  $connection = mysqli_connect('127.0.0.1','root','Amy2000718!','Movie_Database');
+  $connection = mysqli_connect('127.0.0.1','root','','Movie_Database');
   $query = "SELECT title FROM movie_name";
   $result = mysqli_query($connection,$query)
    or die('Error making select users query' . mysql_error());
