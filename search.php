07e4e5e5854049cf359898c8525d1ef39e3e22e7
QiuYuer <zcabyqi@ucl.ac.uk>
2021-03-16 20:58:59 +0000

    search

---
+++ search.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Search page</title>
+  </head>
+  <body>
+	  <h1>Search</h1><br/>
+	<form action="search.php" method="post">
+        <select name="select_option">
+            <option value = "movie_name">Movie Name</option>
+            <option value = "genres">Genre</option>
+            <option value = "tags">Tag</option>
+        </select>
+		<input type="text" name="keywords" placeholder="Input any content you want to search">
+		<input type="submit" value="Submit">
+	</form>
+  </body>
+</html>
+<?php
+    $connection = mysqli_connect('127.0.0.1','root','','Movie_Database');
+    $keywords=$_POST['keywords'];
+    $option = $_POST['select_option'];
+    if(strcmp($option, "movie_name")==0){
+        $sql="SELECT*FROM movies_info WHERE title like '%".$keywords."%' ORDER BY year";
+        $result=mysqli_query($connection,$sql);
+        if(!$result){
+            die('Cannot read data!'.mysqli_error($connection));
+        }
+        echo "<h2>Research Result</h2>";
+        echo "<table border='1'><tr><td><b>Year</b></td><td><b>Movie Name</b></td></tr>";
+        while($row=mysqli_fetch_array($result)){
+            echo '<tr><td>' . $row['year']. '</td><td>' .
+            $row['title']. '</tr></td>'  ;      
+        }
+        echo '</table>';
+    }
+     
+    if(strcmp($option, "tags")==0){
+        $sql="SELECT*FROM tags,movies_info WHERE tag like '%".$keywords."%' AND tags.movieId = movies_info.movieId ORDER BY tag";
+        $result=mysqli_query($connection,$sql);
+        if(!$result){
+            die('Cannot read data!'.mysqli_error($connection));
+        }
+        echo "<h2>Search Result</h2>";
+        echo "<table border='1'><tr><td><b>Tag</b></td><td><b>Year</b></td><td><b>Movie Name</b></td></tr>";
+        while($row=mysqli_fetch_array($result)){
+            echo '<tr><td>' . $row['tag']. '</td><td>' .$row['year'].'</td><td>' .
+            $row['title']. '</tr></td>'  ;  
+        }
+        echo '</table>';
+    }
+
+    if(strcmp($option, "genres")==0){
+        $sql="SELECT*FROM genres,movies_info WHERE genre like '%".$keywords."%' AND genres.movieId = movies_info.movieId ORDER BY year";
+        $result=mysqli_query($connection,$sql);
+        if(!$result){
+            die('Cannot read data!'.mysqli_error($connection));
+        }
+        echo "<h2>Search Result</h2>";
+        echo "<table border='1'><tr><td><b>Genre</b></td><td><b>Year</b></td><td><b>Movie Name</b></td></tr>";
+        while($row=mysqli_fetch_array($result)){
+            echo '<tr><td>' . $row['genre']. '</td><td>' . $row['year'].'</td><td>' .
+            $row['title']. '</tr></td>'  ;  
+        }
+        echo '</table>';
+    }
+    mysqli_close($connection);
+?>
