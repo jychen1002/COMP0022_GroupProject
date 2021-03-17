7a3ad559081c1e1ec9d52491a49d7ee2bcd88d8d
alke <zcabidx@ucl.ac.uk>
2021-03-17 17:14:35 +0000

    Added personalities table

--- Movie_Report.php
+++ Movie_Report.php
@@ -12,7 +12,7 @@
   </body>
 </html>
 <?php
-    $connection = mysqli_connect('127.0.0.1','root','','Movie_Database');
+    $connection = mysqli_connect('127.0.0.1','root','','newDB');
     $keywords=$_POST['keywords'];
     $sql="SELECT*FROM movies_info WHERE title like '%".$keywords."%'";
     $result=mysqli_query($connection,$sql);
@@ -23,25 +23,26 @@
     $title = $row['title'];
     $id = $row['movieId'];
     echo '<h2>'.$title.'</h2>';
-    echo '<b>Year</b>: '.$row['year'].'</h2><br/>';
+
     $query="SELECT*FROM genres WHERE movieId = $id";
     $tags=mysqli_query($connection,$query);
     if(!$tags){
         die('Cannot read data!'.mysqli_error($connection));
     }
-    echo '<b>Genre</b>: ';
+    echo "Genre: ";
     while($row=mysqli_fetch_array($tags)){
         echo $row['genre']. ', '  ;  
     }
 
-    $query="SELECT*FROM tags WHERE movieId = $id";
+    $query="SELECT COUNT(tag) AS TAGCOUNT, tag from tags WHERE movieId = $id GROUP BY tag";
+
     $tags=mysqli_query($connection,$query);
     if(!$tags){
         die('Cannot read data!'.mysqli_error($connection));
     }
     echo "<table border='1'><tr><td><b>Tags</b></td></tr>";
     while($row=mysqli_fetch_array($tags)){
-        echo '<tr><td>' . $row['tag']. '</tr></td>'  ;  
+        echo '<tr><td>' . "${row['tag']} (${row['TAGCOUNT']})". '</tr></td>'  ;  
     }
     echo '</table>';
 
@@ -60,6 +61,45 @@
     $average = $sum / $count;
     echo "<tr><td>".$count."</td><td>".$average."</tr></td>";
     echo '</table>';
+
+
+    $sql = "SELECT COUNT(rating) AS RTCOUNT, 5.0 AS SCORE from ratings WHERE movieId = 318 AND rating = 5.0;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 4.5 AS SCORE from ratings WHERE movieId = 318 AND rating = 4.5;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 4.0 AS SCORE from ratings WHERE movieId = 318 AND rating = 4.0;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 3.5 AS SCORE from ratings WHERE movieId = 318 AND rating = 3.5;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 3.0 AS SCORE from ratings WHERE movieId = 318 AND rating = 3.0;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 2.5 AS SCORE from ratings WHERE movieId = 318 AND rating = 2.5;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 2.0 AS SCORE from ratings WHERE movieId = 318 AND rating = 2.0;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 1.5 AS SCORE from ratings WHERE movieId = 318 AND rating = 1.5;";
+    $sql .= "SELECT COUNT(rating) AS RTCOUNT, 1.0 AS SCORE from ratings WHERE movieId = 318 AND rating = 1.0;";
+
+    $sql.= "SELECT COUNT(rating) AS RTCOUNT, 0.5 AS SCORE, FROM ratings WHERE movieId = 318 AND rating = 0.5;";
+
+    if (mysqli_multi_query($connection, $sql)) {
+      do {
+    // Store first result set
+        if ($result = mysqli_store_result($connection)) {
+           
+          while ($row = mysqli_fetch_assoc($result)) {
+            //echo "<p> title = ${row['title']},  </p>"; 
+            echo "<p> count = ${row['RTCOUNT']}  score = ${row['SCORE']}</p>";
+          }
+          mysqli_free_result($result);
+        }
+    // if there are more result-sets, the print a divider
+        if (mysqli_more_results($connection)) {
+            //printf("-----------------");
+            
+        }
+             //Prepare next result set
+      } while (mysqli_next_result($connection));
+      }
+
+
+
+
+
+
     mysqli_close($connection);
    
 ?>
