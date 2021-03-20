de5e02ac4431092cb4ffb288f7a1a3cee2a02e69
jychen1002 <liao1531639504@126.com>
2021-03-20 05:13:07 +0000

    add header in signin and signup page

--- signin.php
+++ signin.php
@@ -11,8 +11,63 @@
 
     <!-- Main css -->
     <link rel="stylesheet" href="css/stylesign.css">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700;800&display=swap" rel="stylesheet">
+
+    <!-- Css Styles -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" href="css/elegant-icons.css" type="text/css">
+    <link rel="stylesheet" href="css/flaticon.css" type="text/css">
+    <link rel="stylesheet" href="css/nice-select.css" type="text/css">
+    <link rel="stylesheet" href="css/barfiller.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.min.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 <body>
+    <!-- Header Section Begin -->
+    <header class="header header--normal">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3 col-md-3">
+                    <div class="header__logo">
+                        <a href="./index.php"><img src="img/footer-logo.png" width="50%" height="50%" lt=""></a>
+                    </div>
+                </div>
+                <div class="col-lg-9 col-md-9">
+                    
+                    <div class="header__nav">
+                        <nav class="header__menu mobile-menu">
+                            <ul>
+                                <li><form class=" navbar-search">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search movies,tags,and genres..."
+                                            aria-label="Search" aria-describedby="basic-addon2">
+                                        <div class="input-group-append">
+                                            <button class="btn btn-primary" type="button">
+                                                <i class="fas fa-search"></i>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </form></li>
+                                <li><a href="./listing.php">LISTING</a>
+                                <li><a href=#>RANKING</a>
+                                    <ul class="dropdown">
+                                        <li><a href="./popular.html">THE MOST POPULAR FILMS</a></li>
+                                    </ul>
+                            </ul>
+                        </nav>
+                        <div class="header__menu__right">
+                            <a href="signin.php" class="login-btn"><i class="fa fa-user"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
     <div class="main">
         <section class="sign-in">
             <div class="container">
@@ -49,6 +104,16 @@
         <!-- JS -->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="js/main.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nice-select.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/jquery.nicescroll.min.js"></script>
+    <script src="js/jquery.barfiller.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/jquery.slicknav.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="https://kit.fontawesome.com/186b8010d1.js" crossorigin="anonymous"></script>
 </body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 </html>
 
